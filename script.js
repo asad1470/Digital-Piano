1443a66271e529bc2baccba44f978461918061f4
asad1470 <160141504+asad1470@users.noreply.github.com>
2024-02-15 19:23:25 +0500

    index.html

---
+++ script.js
@@ -0,0 +1,207 @@
+ 
+ // Black Keys:
+ 
+const audio1=new Audio();
+audio1.src="./1.wav"
+audio1.currentTime=0;
+const audio2=new Audio();
+audio2.src="./a.wav"
+audio2.currentTime=0;
+const audio3=new Audio();
+audio3.src="./d.wav"
+audio3.currentTime=0;
+const audio4=new Audio();
+audio4.src="./e.wav"
+audio4.currentTime=0;
+const audio5=new Audio();
+audio5.src="./f.wav"
+audio5.currentTime=0;
+const audio6=new Audio();
+audio6.src="./g.wav"
+audio6.currentTime=0;
+const audio7=new Audio();
+audio7.src="./h.wav"
+audio7.currentTime=0;
+const audio8=new Audio();
+audio8.src="./j.wav"
+audio8.currentTime=0;
+
+
+//white keys:
+const audio9=new Audio();
+audio9.src="./k.wav"
+audio9.currentTime=0;
+const audio10=new Audio();
+audio10.src="./l.wav"
+audio10.currentTime=0;
+const audio11=new Audio();
+audio11.src="./o.wav"
+audio11.currentTime=0;
+const audio12=new Audio();
+audio12.src="./p.wav"
+audio12.currentTime=0;
+const audio13=new Audio();
+audio13.src="./s.wav"
+audio13.currentTime=0;
+const audio14=new Audio();
+audio14.src="./t.wav"
+audio14.currentTime=0;
+const audio15=new Audio();
+audio15.src="./u.wav"
+audio15.currentTime=0;
+const audio16=new Audio();
+audio16.src="./w.wav"
+audio16.currentTime=0;
+const audio17=new Audio();
+audio17.src="./88.mp3"
+audio17.currentTime=0;
+const audio18=new Audio();
+audio18.src="./89.mp3"
+audio18.currentTime=0;
+const audio19=new Audio();
+audio19.src="./3.mp3"
+audio19.currentTime=0;
+const audio20=new Audio();
+audio20.src="./4.mp3"
+audio20.currentTime=0;
+const audio21=new Audio();
+audio21.src="./87.mp3"
+audio21.currentTime=0;
+const audio22=new Audio();
+audio22.src="./6.mp3"
+audio22.currentTime=0;
+const audio23=new Audio();
+audio23.src="./86.mp3"
+audio23.currentTime=0;
+const audio24=new Audio();
+audio24.src="./99.mp3"
+audio24.currentTime=0;
+const audio25=new Audio();
+audio25.src="./9.mp3"
+audio25.currentTime=0;
+const audio26=new Audio();
+audio26.src="./10.mp3"
+audio26.currentTime=0;
+const audio27=new Audio();
+audio27.src="./a.mp3"
+audio27.currentTime=0;
+const audio28=new Audio();
+audio28.src="./u.wav"
+audio28.currentTime=0;
+
+//for keys:
+
+document.addEventListener("keydown",function(event){
+    if(event.key==="A"){
+        audio1.currentTime=0;
+        audio1.play();
+    }
+    else if(event.key==="S" ){
+        audio2.currentTime=0;
+        audio2.play();
+    }
+    else if(event.key==="D"){
+        audio3.currentTime=0;
+        audio3.play();
+    }
+    else if(event.key==="W"){
+        audio4.currentTime=0;
+        audio4.play();
+    }
+    else if(event.key==="F"){
+        audio5.currentTime=0;
+        audio5.play();
+    }
+    else if(event.key==="G"){
+        audio6.currentTime=0;
+        audio6.play();
+    }
+    else if(event.key==="E"){
+        audio7.currentTime=0;
+        audio7.play();
+    }
+    else if(event.key==="H"){
+        audio8.currentTime=0;
+        audio8.play();
+    }
+    else if(event.key==="J"){
+        audio9.currentTime=0;
+        audio9.play();
+    }
+    else if(event.key==="R"){
+        audio10.currentTime=0;
+        audio10.play();
+    }
+    else if(event.key==="K"){
+        audio11.currentTime=0;
+        audio11.play();
+    }
+    else if(event.key==="L"){
+        audio12.currentTime=0;
+        audio12.play();
+    }
+    else if(event.key==="T"){
+        audio13.currentTime=0;
+        audio13.play();
+    }
+    else if(event.key==="Z"){
+        audio14.currentTime=0;
+        audio14.play();
+    }
+    else if(event.key==="X"){
+        audio15.currentTime=0;
+        audio15.play();
+    }
+    else if(event.key==="Y"){
+        audio16.currentTime=0;
+        audio16.play();
+    }
+    else if(event.key==="C"){
+        audio17.currentTime=0;
+        audio17.play();
+    }
+    else if(event.key==="V"){
+        audio18.currentTime=0;
+        audio18.play();
+    }
+    else if(event.key==="U"){
+        audio19.currentTime=0;
+        audio19.play();
+    }
+    else if(event.key==="B"){
+        audio20.currentTime=0;
+        audio20.play();
+    }
+    else if(event.key==="N"){
+        audio21.currentTime=0;
+        audio21.play();
+    }
+    else if(event.key==="I"){
+        audio22.currentTime=0;
+        audio22.play();
+    }
+    else if(event.key==="M"){
+        audio23.currentTime=0;
+        audio23.play();
+    }
+    else if(event.key===","){
+        audio24.currentTime=0;
+        audio24.play();
+    }
+    else if(event.key==="O"){
+        audio25.currentTime=0;
+        audio25.play();
+    }
+    else if(event.key==="."){
+        audio26.currentTime=0;
+        audio26.play();
+    }
+    else if(event.key==="/"){
+        audio27.currentTime=0;
+        audio27.play();
+    }
+    else if(event.key===" "){
+        audio28.currentTime=0;
+        audio28.play();
+    }
+});
